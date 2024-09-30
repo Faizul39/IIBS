d2856e773ed7e98a6a8fd58dc43eed3f5a3c4d27
Faizul Haque <Faizulhaque2021@gmail.com>
2024-09-30 17:11:04 +0600

    Project Updated

---
+++ StartupExtension/DatabaseExtensionsHelper.cs
@@ -0,0 +1,28 @@
+﻿using IIBS.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace IIBS.StartupExtension
+{
+    public static class DatabaseExtensionsHelper
+    {
+        public static IServiceCollection AddDatabaseExtensionHelper(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(Opt =>
+            {
+                Opt.UseSqlServer(configuration.GetConnectionString(name:"DefaultConnection"));
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder RunMigration(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+            return app;
+        }
+    }
+}
